6263067ed3060add2c1d4e28acebdeed1358b819
steven85048 <steven85048@gmail.com>
2019-04-10 21:50:27 -0700

    add some scripts to run clamp parsing in parallel

---
+++ scripts/batch_clamp_run.py
@@ -0,0 +1,26 @@
+# After splitting input files into many sub-directories, run the shell script on each of those
+
+import threading
+import os
+import subprocess
+
+BASE_DIR = "inputdata/"
+NUM_SUBDIRECTORIES = 2
+FILE_NAME_BASE = "batch_"
+
+def run_clamp_on_thread( subdirectory_name ):
+    subprocess.call([ "../run_pitt_pipeline.sh", subdirectory_name ]) 
+
+def main():
+    threads = []
+
+    # loop through all the files
+    for x in range(0, NUM_SUBDIRECTORIES):
+        folder = BASE_DIR + FILE_NAME_BASE + str(x)
+        run_clamp_on_thread(folder)
+        threads.append( target = run_clamp_on_thread, args = (folder) )
+
+    for thread in threads:
+        thread.start()
+
+    print("Finished CLAMP!!")
---
+++ scripts/input_data_subdirectory_split.py
@@ -0,0 +1,23 @@
+# This script breaks the input-data into many different subdirectories evenly split
+
+import os, os.path
+
+BASE_DIR = "inputdata/"
+NUM_SUBDIRECTORIES = 2
+FILE_NAME_BASE = "batch_"
+
+file_list = os.listdir( BASE_DIR )
+print(file_list)
+
+# Create the directories
+for x in range(0, NUM_SUBDIRECTORIES):
+    os.mkdir(BASE_DIR + FILE_NAME_BASE + str(x) )
+
+# Create the files
+folder_number = 0
+for file in file_list:
+    renamed = BASE_DIR + FILE_NAME_BASE + str(folder_number) + "/" + file
+    currName = BASE_DIR + file
+
+    os.rename( currName, renamed )
+    folder_number = ( (folder_number + 1) % NUM_SUBDIRECTORIES )
