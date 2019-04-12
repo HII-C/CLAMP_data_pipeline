1137da0a01015d7be499659f75ebf31a86c10655
Ryan Yiu <ryiu012@gmail.com>
2019-04-12 16:40:11 -0700

    Update to consider shell script

--- scripts/test_batch_clamp_run.py
+++ scripts/test_batch_clamp_run.py
@@ -9,6 +9,7 @@ BASE_DIR = "../inputdata/"
 SUBD_MIN = 0
 SUBD_MAX = 4
 FILE_NAME_BASE = "batch_"
+OUTPUT_FOLDER = "../outputdata/batch_completed"
 
 def run_clamp_on_thread( subdirectory_name ):
     subprocess.Popen([ "../run_pitt_pipeline.sh", subdirectory_name, "&" ]) 
@@ -31,7 +32,7 @@ def main():
     # loop through all the files
     for x in range(SUBD_MIN, SUBD_MAX):
         folder = BASE_DIR + FILE_NAME_BASE + str(x)
-        processes.append(subprocess.Popen(["../run_pitt_pipeline.sh", folder]))
+        processes.append(subprocess.Popen(["../run_pitt_pipeline.sh", folder, OUTPUT_FOLDER]))
 
     test_completion( processes )
 
