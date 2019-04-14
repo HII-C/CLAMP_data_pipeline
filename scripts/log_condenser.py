9d02d4d531e3165b567fdfe549c952a5e3321756
steven85048 <steven85048@gmail.com>
2019-04-14 01:16:19 -0700

    wb+

--- scripts/log_condenser.py
+++ scripts/log_condenser.py
@@ -7,7 +7,7 @@ CONDENSED_FILE = PARSE_DIR + "condensed.txt"
 file_names = os.listdir(PARSE_DIR)
 file_names.sort()
 
-with open( CONDENSED_FILE, "wb" ) as outfile:
+with open( CONDENSED_FILE, "wb+" ) as outfile:
     for file in file_names:
         with open( file, "rb" ) as infile:
             outfile.write( infile.read() )
