042540f120771127b9667684b165f71b9f4d17dc
steven85048 <steven85048@gmail.com>
2019-04-14 01:10:07 -0700

    Condensing logs

---
+++ scripts/log_condenser.py
@@ -0,0 +1,14 @@
+import glob
+import os
+
+PARSE_DIR = "../parselogs/"
+CONDENSED_FILE = PARSE_DIR + "condensed.txt"
+
+file_names = os.listdir(PARSE_DIR)
+file_names.sort()
+
+with open( CONDENSED_FILE, "wb" ) as outfile:
+    for file in file_names:
+        outfile.write( file + " Parsing \n\n" )
+        with open( file, "rb" ) as infile:
+            outfile.write( infile.read() )
