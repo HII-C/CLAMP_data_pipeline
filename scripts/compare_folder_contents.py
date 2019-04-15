3cdf998a514418d0313e854605f84a367ef3dbd2
Ryan Yiu <ryiu012@gmail.com>
2019-04-15 13:43:11 -0700

    Update check script

--- scripts/compare_folder_contents.py
+++ scripts/compare_folder_contents.py
@@ -5,10 +5,14 @@ INPUT_PATH = "../inputdata/completed/"
 
 file_dict = {}
 
+input_count = 0
+output_count = 0
 for filename in os.listdir(INPUT_PATH):
   if filename.endswith(".txt"):
     file_dict[filename] = True
+  input_count += 1
 
 for filename in os.listdir(OUTPUT_PATH):
   if filename.endswith(".txt") and not file_dict.get(filename):
     print("Following file is not in completed folder: " + filename)
+  output_count += 1
