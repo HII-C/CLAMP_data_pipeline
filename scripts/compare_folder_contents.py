7408ae60098ef541ebc9919099b20d053128da4f
Ryan Yiu <ryiu012@gmail.com>
2019-04-15 13:45:36 -0700

    Fix the increment

--- scripts/compare_folder_contents.py
+++ scripts/compare_folder_contents.py
@@ -10,12 +10,13 @@ output_count = 0
 for filename in os.listdir(INPUT_PATH):
   if filename.endswith(".txt"):
     file_dict[filename] = True
-  input_count += 1
+    input_count += 1
 
 for filename in os.listdir(OUTPUT_PATH):
   if filename.endswith(".txt") and not file_dict.get(filename):
     print("Following file is not in completed folder: " + filename)
-  output_count += 1
+  if filename.endswith(".txt"):
+    output_count += 1
 
 print("Input count: " + str(input_count))
 print("Output count: " + str(output_count))
